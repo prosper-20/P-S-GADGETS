2786e3a77a66057ceb038efc348ea2144dfae387
prosper-20 <edwardprosper001@gmail.com>
2022-05-25 00:27:58 +0100

    updates

--- store/models.py
+++ store/models.py
@@ -1,3 +1,32 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+
+
+CATEGORY_CHOICES = (
+    ('P', 'Snacks'),
+    ('T', 'Entre'),
+    ('G', 'Drink'),
+    ('A', 'Appetizer'),
+    ('MC', 'Main Course')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    released_on = models.CharField(max_length=100)
+    dimensions = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=3)
+    label =  models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField(upload_to="product_images")
