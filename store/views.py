d12b8b70ecd9189126fb2209f267f66da52a6362
prosper-20 <edwardprosper001@gmail.com>
2022-05-25 01:02:40 +0100

    updates

--- store/views.py
+++ store/views.py
@@ -4,4 +4,17 @@ from .models import Product
 # Create your views here.
 
 def home(request):
-    return render(request, 'store/index.html')
+    products = Product.objects.all()
+    context = {
+        "products": products
+    }
+    return render(request, 'store/index.html', context)
+
+def detail(request, slug=None):
+    product = Product.objects.get(slug=slug)
+
+    context = {
+        "product": product
+    }
+
+    return render(request, 'store/product-details.html', context)
