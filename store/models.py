5c9344d050074b77f8457c0006672a8339464982
prosper-20 <edwardprosper001@gmail.com>
2022-05-25 17:50:57 +0100

    updates

--- store/models.py
+++ store/models.py
@@ -55,6 +55,8 @@ class Product(models.Model):
 
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
--- store/views.py
+++ store/views.py
@@ -35,7 +35,7 @@ class Detail(DetailView):
 
 def add_to_cart(request, slug):
     item = get_object_or_404(Product, slug=slug)
-    order_item = OrderItem.objects.create(item=item)
+    order_item = OrderItem.objects.get_or_create(item=item)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
@@ -44,9 +44,11 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
         else:
-            ordered_date = timezone.now()
-            order = Order.objects.create(user=request.user,
-            ordered_date=ordered_date)
             order.items.add(order_item)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user,
+        ordered_date=ordered_date)
+        order.items.add(order_item)
     return redirect("product-detail", slug=slug)
 
