93220df9e1bf7fe5cbd400d0c6165eb991d07249
prosper-20 <edwardprosper001@gmail.com>
2022-05-26 23:11:30 +0100

    updates

--- store/views.py
+++ store/views.py
@@ -97,7 +97,7 @@ def remove_from_cart(request, slug):
             )[0]
             order.items.remove(order_item)
             messages.info(request, "This item was removed from your cart!")
-            return redirect('product-detail', slug=slug)
+            return redirect('order-summary')
         else:
             messages.info(request, "This item was not in your cart.")
             return redirect("product-detail", slug=slug)
@@ -121,8 +121,11 @@ def remove_single_item_from_cart(request, slug):
                 user=request.user,
                 ordered=False
             )[0]
-            order_item.quantity -= 1
-            order_item.save()
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
             messages.info(request, "This item quantity was updated")
             return redirect('order-summary')
         else:
