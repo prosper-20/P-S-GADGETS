d70e6953f37c371d26f3ef906b05522b7f00063c
prosper-20 <edwardprosper001@gmail.com>
2022-05-27 01:08:58 +0100

    updates

--- store/forms.py
+++ store/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.forms import widgets
 from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
 
 PAYMENT_CHOICES = (
     ('S', 'Stripe'),
@@ -11,12 +13,14 @@ class CheckoutForm(forms.Form):
     'placeholder': '1234 Main Street'}))
     apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
     'placeholder': 'Apartment or Suite'}))
-    country = CountryField(blank_label='(select country)').formfield(attrs={
+    country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
         'class': 'custom-select d-block w-100'
-    })
-    zip = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
+    }))
+    zip = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    same_billing_address = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
     payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
 
 
--- store/views.py
+++ store/views.py
@@ -27,9 +27,13 @@ class CheckoutView(View):
 
     def post(self, *args, **kwargs):
         form = CheckoutForm(self.request.POST or None)
+        print(self.request.POST)
         if form.is_valid():
+            print(form.cleaned_data)
             print("The form is valid")
             return redirect("checkout")
+        messages.warning(self.request, "Failed Checkut")
+        return redirect('checkout')
         
 
 
