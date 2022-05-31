35d5300f4abb9dc43e7750e4e617e8034f68cf7e
prosper-20 <edwardprosper001@gmail.com>
2022-05-31 16:35:38 +0100

    updates

--- store/models.py
+++ store/models.py
@@ -109,7 +109,7 @@ class Order(models.Model):
 class BillingAddress(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     street_address = models.CharField(max_length=100)
-    aprtment_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
     country = CountryField(multiple=False)
     zip = models.CharField(max_length=100)
 
