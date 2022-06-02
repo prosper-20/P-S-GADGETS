f9dccba0d628e0c1504f486b6b134be6588a659f
prosper-20 <edwardprosper001@gmail.com>
2022-06-02 23:13:08 +0100

    updates

--- store/forms.py
+++ store/forms.py
@@ -1,3 +1,5 @@
+from pickle import FALSE
+import re
 from django import forms
 from django.forms import widgets
 from django_countries.fields import CountryField
@@ -9,16 +11,12 @@ PAYMENT_CHOICES = (
 )
 
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField(widget=forms.TextInput(attrs={
-    'placeholder': '1234 Main Street'}))
-    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
-    'placeholder': 'Apartment or Suite'}))
-    country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
+    shipping_address = forms.CharField(required=False)
+    ashipping_address2 = forms.CharField(required=False)
+    shipping_country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
         'class': 'custom-select d-block w-100'
     }))
-    zip = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control'
-    }))
+    shipping_zip = forms.CharField(required=False)
     same_shipping_address = forms.BooleanField(required=False)
     save_info = forms.BooleanField(required=False)
     payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
