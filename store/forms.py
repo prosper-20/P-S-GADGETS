9ec90995d26c0c07457cda30ed8ee56d91b1d16c
prosper-20 <edwardprosper001@gmail.com>
2022-06-02 23:45:28 +0100

    updates

--- store/forms.py
+++ store/forms.py
@@ -1,7 +1,8 @@
 from pickle import FALSE
 import re
+from xmlrpc.client import Boolean
 from django import forms
-from django.forms import widgets
+from django.forms import BooleanField, widgets
 from django_countries.fields import CountryField
 from django_countries.widgets import CountrySelectWidget
 
@@ -12,13 +13,30 @@ PAYMENT_CHOICES = (
 
 class CheckoutForm(forms.Form):
     shipping_address = forms.CharField(required=False)
-    ashipping_address2 = forms.CharField(required=False)
-    shipping_country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
+    shipping_address2 = forms.CharField(required=False)
+    shipping_country = CountryField(blank_label='(select country)').formfield(
+        required=False,
+        widget=CountrySelectWidget(attrs={
         'class': 'custom-select d-block w-100'
     }))
     shipping_zip = forms.CharField(required=False)
-    same_shipping_address = forms.BooleanField(required=False)
-    save_info = forms.BooleanField(required=False)
+   
+
+    billing_address = forms.CharField(required=False)
+    billing_address2 = forms.CharField(required=False)
+    billing_country = CountryField(blank_label='(select country)').formfield(
+        required=False,
+        widget=CountrySelectWidget(attrs={'class': 'custom-select d-block w-100',
+        }))
+    billing_zip = forms.BooleanField(required=False)
+    same_billing_address = forms.BooleanField(required=False)
+    set_default_shipping = forms.BooleanField(required=False)
+    use_default_shipping = forms.BooleanField(required=False)
+    set_default_billing = forms.BooleanField(required=False)
+    use_default_billing = forms.BooleanField(required=False)
+
+    
+
     payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
 
 
