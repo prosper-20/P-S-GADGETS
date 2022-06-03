67fa3871778d54f6663508776d9b41bb578c0002
prosper-20 <edwardprosper001@gmail.com>
2022-06-03 01:18:33 +0100

    updates

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -18,3 +19,20 @@ def register(request):
         "form": form
     }
     return render(request, 'users/register.html', context)
+
+
+def register2(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(usermame=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect("register")
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken already')
+            
+
