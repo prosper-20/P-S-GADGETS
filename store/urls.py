95210041627251c7296e36db9760ff59e6381ad9
prosper-20 <edwardprosper001@gmail.com>
2022-06-06 23:39:45 +0100

    updates

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from store.models import Order
 from . import views
-from .views import add_to_cart, remove_from_cart, OrderSummaryView, remove_single_item_from_cart, CheckoutView, PaymentView, HomeView, AddCouponView, RequestRefundView, phone_view, accessories_view, computing_view, camera_view, tablets_view, discount_view
+from .views import add_to_cart, remove_from_cart, OrderSummaryView, remove_single_item_from_cart, CheckoutView, PaymentView, HomeView, AddCouponView, RequestRefundView, phone_view, accessories_view, computing_view, camera_view, tablets_view, discount_view, food_view
 
 urlpatterns = [
     # path("", views.Home.as_view(), name="home"),
@@ -22,5 +22,6 @@ urlpatterns = [
     path("category/tablets/", tablets_view, name="tablets-view"),
     path("category/camera/", camera_view, name="camera-view"),
     path("category/computing/", computing_view, name="computing-view"),
+    path("category/food-beverages/", food_view, name="food-view"),
     path("product/discount/", discount_view, name="discount-view")
 ]
