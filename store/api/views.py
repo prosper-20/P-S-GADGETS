aac9d3e9529ab27d810ad3d0c700bd704d03c981
prosper-20 <edwardprosper001@gmail.com>
2022-06-14 20:29:18 +0100

    updates

--- store/api/views.py
+++ store/api/views.py
@@ -25,4 +25,41 @@ def api_detail_view(request, slug):
     except Product.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    
+    if request.method == "GET":
+        serializer = ProductSerializers(product)
+        return Response(serializer.data)
+
+@api_view(['PUT'])
+def api_update_view(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "PUT":
+        serializer = ProductSerializers(product, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data["sucess"] = "update successful"
+        else:
+            data["failure"] = "update failed"
+        return Response(data)
+
+
+@api_view(['DELETE'])
+def api_delete_view(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "DELETE":
+        operation = product.delete()
+        data = {}
+        if operation:
+            data["sucess"] = "delete sucessful"
+        else:
+            data['failure'] = "delete failed"
+        return Response(data)
+
