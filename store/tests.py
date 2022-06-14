cd3c58f74837972e35a463a3ccc52cc00236b6ea
prosper-20 <edwardprosper001@gmail.com>
2022-06-14 18:01:54 +0100

    updates

--- store/tests.py
+++ store/tests.py
@@ -1,6 +1,7 @@
 from urllib import response
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your tests here.
 
@@ -39,3 +40,35 @@ class StorePage(TestCase):
     # def test_productpage_contains(self):
     #     response = self.client.get(reverse("product-detail"))
     #     self.assertContains(response, "P's GADGETS")
+
+
+class UserPageTest(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create_user(
+            username = 'esther',
+            email = 'esther@gmail.com',
+            password = 'testing321'
+        )
+        self.assertEqual(user.username, 'esther')
+        self.assertEqual(user.email, 'esther@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    
+    def test_user_registration_status_code(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_registration_template(self):
+        response = self.client.get(reverse("login"))
+        self.assertTemplateUsed(response, 'users/login.html')
+    
+    def test_user_registration_page_contains(self):
+        response = self.client.get(reverse("login"))
+        self.assertContains(response, "P's Gadgets | Sign In")
+
+    def test_user_registration_page_not_contained(self):
+        response = self.client.get(reverse("login"))
+        self.assertNotContains(response, 'This is John')
